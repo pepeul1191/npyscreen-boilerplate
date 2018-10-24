080ccfbbd6c05d09f0ff3773e147fc5a5522637b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-24 11:08:00 -0400

    Print log de error y sólo print

--- configs/log.py
+++ configs/log.py
@@ -5,13 +5,13 @@ import logging
 from datetime import datetime
 
 def log_message(message):
-  rpta = ''
-  rpta = ' ' + str(datetime.now()) + '\n \t' + message
+  rpta = 'PRINT \n'
+  rpta = rpta + ' ' + str(datetime.now()) + '\n \t' + message
   logging.basicConfig(filename = 'debug.log',level = logging.DEBUG)
   logging.debug(rpta)
 
 def log_error(error):
-  rpta = ''
-  rpta = ' ' + str(datetime.now()) + '\n \t' + error
+  rpta = 'ERROR \n'
+  rpta = rpta + ' ' + str(datetime.now()) + '\n \t' + error
   logging.basicConfig(filename = 'debug.log', level = logging.DEBUG)
   logging.debug(rpta)
