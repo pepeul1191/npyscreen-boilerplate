e2a30f6555387e5812a3f52ea1ef213ea89e6bb7
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-24 10:37:36 -0400

    Empezando log file

--- app.py
+++ app.py
@@ -2,11 +2,14 @@
 # encoding: utf-8
 
 import npyscreen
+from config.log import add
 
 def myFunction(*args):
   F = npyscreen.Form(name='My Test Application')
+  F.add(npyscreen.TitleText, name="First Widget")
   F.edit()
 
 if __name__ == '__main__':
   npyscreen.wrapper_basic(myFunction)
+  add()
   print("Blink and you missed it!")
